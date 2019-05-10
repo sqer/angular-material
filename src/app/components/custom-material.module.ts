c663021e42d73500daf2aaaf5cedce0ecbf79e96
Tomasz Sapiński <tomasz.sapinski@lhsystems.com>
2019-05-10 10:03:21 +0200

    Init Angular with material design

---
+++ src/app/components/custom-material.module.ts
@@ -0,0 +1,9 @@
+import {MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
+import {NgModule} from "@angular/core";
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule],
+})
+export class MyOwnCustomMaterialModule {
+}
